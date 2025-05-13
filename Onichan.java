a726777c95dcdacf8156b5884b358e88aea52435
AlbertoGenoxius <20243rd044@utez.edu.mx>
2025-05-13 11:37:10 -0600

    Dias de la semana

---
+++ Onichan.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Onichan {
+    public static void main(String[] args) {
+        Scanner UWU = new Scanner(System.in);
+        System.out.println("Ingresa el numero dia de la semana : ");
+        int dia = UWU.nextInt();
+        if (dia == 1) {
+            System.out.println("Lunes");
+        }else if (dia == 2) {
+            System.out.println("Martes");
+        }else if (dia == 3) {
+            System.out.println("Miercoles");
+        }else if (dia == 4) {
+            System.out.println("Jueves");
+        }else if (dia == 5) {
+            System.out.println("Viernes");
+        }else if (dia == 6) {
+            System.out.println("Sabado");
+        }else if (dia == 7) {
+            System.out.println("Domingo");
+        }else{
+            System.out.println("Dia invalido");
+        }
+    }
+}
